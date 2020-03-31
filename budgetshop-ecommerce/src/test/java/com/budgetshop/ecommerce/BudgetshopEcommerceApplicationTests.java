c422fbe5517f771e103d2dbc58ea2b5263dbf847
Hp <Hp@DESKTOP-6I9B3D9>
2020-04-01 00:20:51 +0530

    first commit-10-04-2020 - 12:20 A.M.

---
+++ budgetshop-ecommerce/src/test/java/com/budgetshop/ecommerce/BudgetshopEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetshop.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetshopEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
